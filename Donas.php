a51a0122823da1e3b49e424c2fd6ac2a9dfabfd2
Your Name <you@example.com>
2023-06-28 13:08:03 -0600

    Vista de donas

--- Donas.php
+++ Donas.php
@@ -1,3 +1,29 @@
 <?php include ("templates/cabeza.php");?>
 <h1>Donas</h1>
+<div class="Fila-1">
+    <div class="ejemplo1">
+    <img src="img/donas_de_azucar.jpg" alt="" class="Dona">
+    <br>
+    <p>Dona de azucar</p>
+    <p>$13.00 </p>
+    </div>
+    <div class="ejemplo2">
+    <img src="img/DonasChocolate.jpeg" alt="">
+    <br>
+    <p>Donas glaseado chocolate</p>
+    <p>13.00</p>
+    </div>
+    <div class="ejemplo3">
+    <img src="img/DonasFresa.jpeg" alt="" class="Concha">
+    <p>Donas glaseado fresa</p>
+    <p>13.00</p>
+    </div>
+</div>
+<div class="Fila-2">
+</div>
+<div class="paginas">
+    <p class="p1">1</p>
+    <p class="p2">2</p>
+    <p class="p3">3</p>
+</div>
 <?php include ("templates/pie.php");?>
