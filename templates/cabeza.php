d89cd7dc97ac13f01ba0632163b1028cb151d9a2
Víctor Emmanuel <cabellordz15@gmail.com>
2023-06-27 20:21:54 -0600

    cascaronsito

---
+++ templates/cabeza.php
@@ -0,0 +1,20 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilo/style.css">
+    <title>Lagupanificadora</title>
+</head>
+<body>
+    <nav>
+        <div class="barra">
+            <ul>
+                <li><a href="bolillos.php">Bolillos</a></li>
+                <li><a href="donas.php">Donas</a></li>
+                <li><a href="conchas.php">Conchas</a></li>
+                <li><a href="empanadas.php">Empanadas</a></li>
+                <a href="login.php"><img src="img/perfil.png"></a>
+            </ul>
+        </div>
+    </nav>
+</div>
