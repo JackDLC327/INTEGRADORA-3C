9c37525b56dd4c0d644d03f43d2a833300d41cbc
Víctor Emmanuel <cabellordz15@gmail.com>
2023-06-28 10:46:49 -0600

    login otra ves porque se borro

---
+++ login.php
@@ -0,0 +1,19 @@
+<?php include("templates/cabeza.php");?>
+<form class="formulario">
+    <h1>Regsitrate</h1>
+    <div class=form>
+        <input type="text" placeholder="name" ><br><br>
+        <input type="text" placeholder="email"><br><br>
+        <input type="text" placeholder="passwd"><br><br>
+        <p>Tus datos estan seguros en el baul de bolillo no te preocupes</p>
+    </div>
+    <form class="login"></form>
+    <h1>Login</h1>
+    <div>
+        <input type="text" placeholder="email"><br><br>
+        <input type="text" placeholder="email"><br><br>
+        <p>¿No tienes una cuenta? Registrate aqui...<a style="font-size: 20px;" href="donas.php">Registrate</a></p>
+    </div>
+
+</form>
+<?php include("templates/pie.php");?>
